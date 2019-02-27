ce2393a766082498bacb542de9aba8bc2b4bfbe6
Mitch-Tyrer <mltcinematography@gmail.com>
2019-02-26 18:38:38 -0700

    created ref to refactor player form

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -2,27 +2,21 @@ import React, { Component } from 'react';
 
 class AddPlayerForm extends Component {
 
-    state = {
-        value: ''
-    }
 
-    handleValueChange = (e) => {
-        this.setState({ value: e.target.value });
-    }
+    playerInput = React.createRef()
 
     handleSubmit = (e) => {
         e.preventDefault();
-        this.props.addPlayer(this.state.value);
-        this.setState({value: ''});
+        this.props.addPlayer(this.playerInput.current.value);
+        e.currentTarget.reset();
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <input type="text" 
+                ref={this.playerInput}
                 placeholder="Enter a Player's Name" 
-                value={this.state.value}
-                onChange={this.handleValueChange}
                 />
                 <input 
                 type="submit" 
